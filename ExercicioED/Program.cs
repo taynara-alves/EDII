93f529ae8a4cc40181b0a9dcf5f580598744dd3c
taynara-alves <maple.pony@hotmail.com>
2018-08-16 20:00:58 -0300

    primeiro

---
+++ ExercicioED/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioED
+{
+    class Program
+    {
+        class Tempo
+        {
+            public int qtde;
+            public double[,] tempo = new double[15, 3];
+
+            public void entraTempos()
+            {
+               
+
+                for (int l = 0; l < 3; l++)
+                {
+
+                    Console.WriteLine((l+1) + " volta");
+
+                    for (int i = 0; i < 15; i++)
+                    {
+                        double temp = 0;
+                        Console.WriteLine("tempo do carro " + (i + 1));
+                        temp = Convert.ToDouble(Console.ReadLine());
+                        tempo[i, l] = temp;
+
+
+
+                    }
+                }
+
+                for (int l = 0; l < 3; l++)
+                {
+                    Console.WriteLine("\n\n" + (l+1) + " volta");
+                    for (int i = 0; i < 15; i++)
+                    {
+                        Console.WriteLine("\n"+tempo[i, l]+"\n");
+
+                    }
+                }
+
+
+            }
+
+            public void mediaCarros()
+            {
+
+            }
+
+            public void mediaVoltas()
+            {
+
+            }
+
+            public void montaGrid()
+            {
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+
+            Tempo tempo = new Tempo();
+
+            tempo.entraTempos();
+
+            Console.ReadKey();
+            
+
+        }
+
+    
+    }
+}
---
+++ ExercicioED/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("ExercicioED")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExercicioED")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("6cd4c05a-11cf-42b1-b855-04a00e492a33")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
